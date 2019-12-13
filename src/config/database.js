7ff68853596f1ca20c43e017a743a298bf92a9b0
vitorkaio <vitorcaiodepaula@gmail.com>
2019-12-13 10:48:23 -0300

    elephantsql

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: '123456',
-  database: 'sqlnode',
+  host: 'tuffi.db.elephantsql.com', // https://customer.elephantsql.com/instance
+  username: 'tmvcrcbj',
+  password: 'Ls0zN1SYRx_PnadD_m4AwJydflFZriXj',
+  database: 'tmvcrcbj',
   define: {
     timestamps: true, // add fields create_at and updated_at
     underscored: true // name table and columns with: table_name
